e427f0a0a4b1c8cbb06d4313507c537ad9b9167b
Yuri Andrianov <andrianov.yuri.spb@gmail.com>
2022-03-08 13:35:54 +0300

    Initial Commit

---
+++ excursionsTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  excursionsTestTask
+//
+//  Created by Юрий Андрианов on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
